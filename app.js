92822d50c95c96e904c4490449a7cf044b0bb15f
Min Kookie <min03041999@gmail.com>
2022-11-07 10:12:26 +0700

    feat(source): deploy version 3

--- app.js
+++ app.js
@@ -60,7 +60,7 @@ app.use((req, res, next) => {
     "Access-Control-Allow-Methods",
     "OPTIONS, GET, POST, PUT, PATCH, DELETE"
   );
-  res.setHeader("Access-Control-Allow-Origin", "Content-Type, x-access-token");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, x-access-token");
   next();
 });
 
